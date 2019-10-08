40ce66e61277daaf9c9f8c35cf649f4b4b27d9c0
Andres Decastro <af.decastro879@gmail.com>
2019-10-08 10:30:57 +0200

    Fixing script for github release

--- bin/ci/github_release.sh
+++ bin/ci/github_release.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(dirname "$0")"
 source "${DIR}/include.sh"
 
-if [[ "${TRAVIS_BRANCH}" = "master" ]]
+if [[ -n "${TRAVIS_TAG}" ]]
 then
     curl -v -d "{\"tag_name\": \"$TRAVIS_TAG\"}" -H "Authorization: token ${GITHUB_KEY}" -X POST "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases"
 fi
