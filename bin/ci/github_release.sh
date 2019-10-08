508999becdfad5c62b9c68ddc215e9830fe71afb
Andres Decastro <af.decastro879@gmail.com>
2019-10-08 10:40:32 +0200

    Conditional build base on tag regex

--- bin/ci/github_release.sh
+++ bin/ci/github_release.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(dirname "$0")"
 source "${DIR}/include.sh"
 
-if [[ -n "${TRAVIS_TAG}" ]]
+if [[ -n "${TRAVIS_TAG}" && ${TRAVIS_TAG} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
 then
     curl -v -d "{\"tag_name\": \"$TRAVIS_TAG\"}" -H "Authorization: token ${GITHUB_KEY}" -X POST "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases"
 fi
